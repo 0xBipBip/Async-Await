a9981c8bf04d27393f1d718ad16a500b643e2523
0xBipBip <oxbipbip@gmail.com>
2023-05-29 15:24:32 -0300

    theory

--- async.js
+++ async.js
@@ -9,3 +9,12 @@ async function example() {
 }
 
 example();
+
+//In this example, the delay function returns a promise that resolves after the specified 
+//number of milliseconds. The example function is defined as an async function. Within this 
+//function, the await keyword is used to pause execution for 2000 milliseconds while waiting 
+//for the delay promise to resolve. This makes it appear as if the code is synchronous, and 
+//the "End" message is logged after the delay.
+
+//Note that the use of await is only valid inside an async function. If you try to use await 
+//outside of an async function, you'll encounter a syntax error.
